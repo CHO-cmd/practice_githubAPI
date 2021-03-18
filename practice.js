a1bc2e26746b14e5cd6a0ce76099f94eb005af7f
CHO-cmd <wjdqls9362@gmail.com>
2021-03-18 17:57:59 +0900

    successed using graphQL API

--- practice.js
+++ practice.js
@@ -4,8 +4,28 @@ const axios = require("axios");
 // https://api.github.com/graphql
 
 async function getData() {
-  let what = await axios.get("https://api.github.com/graphql", {
+  const endpoint = "https://api.github.com/graphql";
+  const query = `query { 
+    repository (owner:"codestates", name:"help-desk") { 
+      issues {
+        totalCount
+      }
+    }
+  }`;
+
+  // query {
+  //   repository (owner:"codestates", name:"help-desk") {
+  //     issue {
+  //       author {
+  //         login(:"CHO-cmd")
+  //       }
+  //     }
+  //   }
+  // }
+
+  let what = await axios.get(endpoint, {
     headers: { Authorization: `bearer ${process.env.GITHUB_TOKEN}` },
+    body: JSON.stringify({ query }),
   });
 
   console.log(what);
